20fafcef4a7e03bfb9429813cf49b15003b6cfb5
sudharsan429 <sudharsanarulraj6@gmail.com>
2026-08-17 11:27:32 +0530

    Update API base URL to production endpoint

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const apiBaseUrl = 'https://rithanya-enterprises-cms.onrender.com/api';
 
 const api = axios.create({
   baseURL: apiBaseUrl,
